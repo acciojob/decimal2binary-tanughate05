40c89cc5a35f308b03d0ce9dc8db14ad9764aac9
tanughate05 <93458822+tanughate05@users.noreply.github.com>
2023-07-19 16:11:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function decimalToBinary(num) {
   // Write your code here
-	console.log(num.toString(10));
+	  let binary = new Array(32);
+
+  let i = 0;
+  while (num > 0) {
+    binary[i] = num % 2;
+    num= Math.floor(num / 2);
+    i++;
+  }
+
+  for (let j = i - 1; j >= 0; j--)
+    document.write(binary[j]);
+
 }
 
 window.decimalToBinary = decimalToBinary;
